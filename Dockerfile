c7d204cb7c9deadd28c760e0683418a011e60f39
nitin.sharma <nitin.sharma@foodpanda.in>
2018-02-02 20:18:41 +0530

    NO_TICKET	leader with follower on 5.3
    Signed-off-by: nitin.sharma <nitin.sharma@foodpanda.in>

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM ubuntu:vivid
+FROM ubuntu:latest
 
 RUN apt-get update \
  && apt-get -y install git ant openjdk-8-jdk \
@@ -6,8 +6,9 @@ RUN apt-get update \
 RUN mkdir /tmp/zookeeper
 WORKDIR /tmp/zookeeper
 RUN git clone https://github.com/apache/zookeeper.git .
-RUN git checkout release-3.5.1
+RUN git checkout release-3.5.3
 RUN ant jar
 ADD zoo.cfg /tmp/zookeeper/conf/
 ADD zk-init.sh /usr/local/bin/
+EXPOSE 2181 2888 3888
 ENTRYPOINT ["/usr/local/bin/zk-init.sh"]
--- zk-init.sh
+++ zk-init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-ZK=$1
+#ZK=$1
+sleep 3
 IPADDRESS=`ip -4 addr show scope global dev eth0 | grep inet | awk '{print \$2}' | cut -d / -f 1`
 MYID=$(echo $IPADDRESS | sed 's/\.//g')
 
@@ -9,6 +10,9 @@ if [ -n "$ZK" ]
 then
   ./bin/zkCli.sh -server $ZK:2181 get /zookeeper/config | grep ^server >> /tmp/zookeeper/conf/zoo.cfg
   echo "server.$MYID=$IPADDRESS:2888:3888:observer;2181" >> /tmp/zookeeper/conf/zoo.cfg
+  echo "reconfigEnabled=true" >> /tmp/zookeeper/conf/zoo.cfg
+  echo "skipACL=yes" >> /tmp/zookeeper/conf/zoo.cfg
+  echo "4lw.commands.whitelist=stat, ruok, conf, isro" >> /tmp/zookeeper/conf/zoo.cfg
   cp /tmp/zookeeper/conf/zoo.cfg /tmp/zookeeper/conf/zoo.cfg.org
   /tmp/zookeeper/bin/zkServer-initialize.sh --force --myid=$MYID
   ZOO_LOG_DIR=/var/log ZOO_LOG4J_PROP='INFO,CONSOLE,ROLLINGFILE' /tmp/zookeeper/bin/zkServer.sh start
@@ -17,6 +21,9 @@ then
   while true; do sleep 10000; done # block forever
 else
   echo "server.$MYID=$IPADDRESS:2888:3888;2181" >> /tmp/zookeeper/conf/zoo.cfg
+  echo "reconfigEnabled=true" >> /tmp/zookeeper/conf/zoo.cfg
+  echo "skipACL=yes" >> /tmp/zookeeper/conf/zoo.cfg
+  echo "4lw.commands.whitelist=stat, ruok, conf, isro" >> /tmp/zookeeper/conf/zoo.cfg
   /tmp/zookeeper/bin/zkServer-initialize.sh --force --myid=$MYID
   ZOO_LOG_DIR=/var/log ZOO_LOG4J_PROP='INFO,CONSOLE,ROLLINGFILE' /tmp/zookeeper/bin/zkServer.sh start-foreground
 fi
